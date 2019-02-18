8c72eb6e81603f03983831d2f6fb04e786a1c3b9
Dave <solenovex@live.com>
2019-02-18 13:53:23 +0800

    add implicit flow, an angular client skeleton

---
+++ 04. Implicit Flow - Angular SPA Client/Idp/Config.cs
@@ -0,0 +1,100 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+using IdentityServer4;
+
+namespace Idp
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Address(),
+                new IdentityResources.Phone(),
+                new IdentityResources.Email()
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new ApiResource[]
+            {
+                new ApiResource("api1", "My API #1")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                // client credentials flow client
+                new Client
+                {
+                    ClientId = "console client",
+                    ClientName = "Client Credentials Client",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+
+                    AllowedScopes = { "api1" }
+                },
+
+                // wpf client, password grant
+                new Client
+                {
+                    ClientId = "wpf client",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    ClientSecrets =
+                    {
+                        new Secret("wpf secrect".Sha256())
+                    },
+                    AllowedScopes = {
+                        "api1",
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Email,
+                        IdentityServerConstants.StandardScopes.Address,
+                        IdentityServerConstants.StandardScopes.Phone,
+                        IdentityServerConstants.StandardScopes.Profile }
+                },
+
+                // mvc client, authorization code
+                new Client
+                {
+                    ClientId = "mvc client",
+                    ClientName = "ASP.NET Core MVC Client",
+
+                    AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                    ClientSecrets = { new Secret("mvc secret".Sha256()) },
+
+                    RedirectUris = { "http://localhost:5002/signin-oidc" },
+
+                    FrontChannelLogoutUri = "http://localhost:5002/signout-oidc",
+                    PostLogoutRedirectUris = { "http://localhost:5002/signout-callback-oidc" },
+                    
+                    AlwaysIncludeUserClaimsInIdToken = true,
+
+                    AllowOfflineAccess = true, // offline_access
+                    AccessTokenLifetime = 60, // 60 seconds
+
+                    AllowedScopes =
+                    {
+                        "api1",
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Email,
+                        IdentityServerConstants.StandardScopes.Address,
+                        IdentityServerConstants.StandardScopes.Phone,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
+                },
+            };
+        }
+    }
+}
---
+++ 04. Implicit Flow - Angular SPA Client/WpfClient/WpfClient/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using IdentityModel.Client;
+
+namespace WpfClient
+{
+    public partial class MainWindow : Window
+    {
+        private string _accessToken;
+        private DiscoveryResponse _disco;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void RequestAccessToken_ButtonClick(object sender, RoutedEventArgs e)
+        {
+            var userName = UserNameInput.Text;
+            var password = PasswordInput.Password;
+
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000/");
+            _disco = disco;
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            // request access token
+            var tokenResponse = await client.RequestPasswordTokenAsync(new PasswordTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "wpf client",
+                ClientSecret = "wpf secrect",
+                Scope = "api1 openid profile address phone email",
+
+                UserName = userName,
+                Password = password
+            });
+
+            if (tokenResponse.IsError)
+            {
+                MessageBox.Show(tokenResponse.Error);
+                return;
+            }
+
+            _accessToken = tokenResponse.AccessToken;
+            AccessTokenTextBlock.Text = tokenResponse.Json.ToString();
+        }
+
+        private async void RequestApi1Resource_ButtonClick(object sender, RoutedEventArgs e)
+        {
+            // call API1 Resource
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(_accessToken);
+
+            var response = await apiClient.GetAsync("http://localhost:5001/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                MessageBox.Show(response.StatusCode.ToString());
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Api1ResponseTextBlock.Text = content;
+            }
+
+        }
+
+        private async void RequestIdentityResource_ButtonClick(object sender, RoutedEventArgs e)
+        {
+            // call Identity Resource from Identity Server
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(_accessToken);
+
+            var response = await apiClient.GetAsync(_disco.UserInfoEndpoint);
+            if (!response.IsSuccessStatusCode)
+            {
+                MessageBox.Show(response.StatusCode.ToString());
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                IdentityResponseTextBlock.Text = content;
+            }
+        }
+    }
+}
