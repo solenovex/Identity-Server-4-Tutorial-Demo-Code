69de4f239d999375453718e9577e2b01b1af8a5a
Dave Young <solenovex@live.com>
2019-02-22 18:07:50 +0800

    update

---
+++ 07-08. Hybrid Flow - MVC Client/HybridMvcClient/HybridClient/Controllers/HomeController.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HybridClient.Models;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
+namespace HybridClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var user = User.Identity;
+            return View();
+        }
+
+        [Authorize(Roles = "管理员,普通用户")]
+        public async Task<IActionResult> AccessApi1()
+        {
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000/");
+            if (disco.IsError)
+            {
+                throw new Exception(disco.Error);
+            }
+
+            var accessToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+
+            client.SetBearerToken(accessToken);
+            var response = await client.GetAsync("http://localhost:5001/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                if (response.StatusCode == HttpStatusCode.Unauthorized)
+                {
+                    await RenewTokensAsync();
+                    return RedirectToAction();
+                }
+
+                throw new Exception(response.ReasonPhrase);
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            return View("AccessApi1", content);
+        }
+
+        // [Authorize(Roles = "管理员")]
+        [Authorize(Policy = "SmithInSomewhere")]
+        public async Task<IActionResult> Privacy()
+        {
+            var accessToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+            var idToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.IdToken);
+
+            var refreshToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.RefreshToken);
+            // var code = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.Code);
+
+            ViewData["accessToken"] = accessToken;
+            ViewData["idToken"] = idToken;
+            ViewData["refreshToken"] = refreshToken;
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task Logout()
+        {
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000/");
+            if (disco.IsError)
+            {
+                throw new Exception(disco.Error);
+            }
+
+            var accessToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+            if (!string.IsNullOrWhiteSpace(accessToken))
+            {
+                var revokeAccessTokenResponse = await client.RevokeTokenAsync(new TokenRevocationRequest
+                {
+                    Address = disco.RevocationEndpoint,
+                    ClientId = "hybrid client",
+                    ClientSecret = "hybrid secret",
+                    Token = accessToken
+                });
+
+                if (revokeAccessTokenResponse.IsError)
+                {
+                    throw new Exception("Access Token Revocation Failed: " + revokeAccessTokenResponse.Error);
+                }
+            }
+
+            var refreshToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.RefreshToken);
+            if (!string.IsNullOrWhiteSpace(refreshToken))
+            {
+                var revokeRefreshTokenResponse = await client.RevokeTokenAsync(new TokenRevocationRequest
+                {
+                    Address = disco.RevocationEndpoint,
+                    ClientId = "hybrid client",
+                    ClientSecret = "hybrid secret",
+                    Token = refreshToken
+                });
+
+                if (revokeRefreshTokenResponse.IsError)
+                {
+                    throw new Exception("Refresh Token Revocation Failed: " + revokeRefreshTokenResponse.Error);
+                }
+            }
+
+            //await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            //await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+        }
+
+        private async Task<string> RenewTokensAsync()
+        {
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                throw new Exception(disco.Error);
+            }
+
+            var refreshToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.RefreshToken);
+
+            // Refresh Access Token
+            var tokenResponse = await client.RequestRefreshTokenAsync(new RefreshTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "mvc client",
+                ClientSecret = "mvc secret",
+                Scope = "api1 openid profile email phone address",
+                GrantType = OpenIdConnectGrantTypes.RefreshToken,
+                RefreshToken = refreshToken
+            });
+
+            if (tokenResponse.IsError)
+            {
+                throw new Exception(tokenResponse.Error);
+            }
+
+            var expiresAt = DateTime.UtcNow + TimeSpan.FromSeconds(tokenResponse.ExpiresIn);
+
+            var tokens = new[]
+            {
+                new AuthenticationToken
+                {
+                    Name = OpenIdConnectParameterNames.IdToken,
+                    Value = tokenResponse.IdentityToken
+                },
+                new AuthenticationToken
+                {
+                    Name = OpenIdConnectParameterNames.AccessToken,
+                    Value = tokenResponse.AccessToken
+                },
+                new AuthenticationToken
+                {
+                    Name = OpenIdConnectParameterNames.RefreshToken,
+                    Value = tokenResponse.RefreshToken
+                },
+                new AuthenticationToken
+                {
+                    Name = "expires_at",
+                    Value = expiresAt.ToString("o", CultureInfo.InvariantCulture)
+                }
+            };
+
+            // 获取身份认证的结果，包含当前的pricipal和properties
+            var currentAuthenticateResult =
+                await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            // 把新的tokens存起来
+            currentAuthenticateResult.Properties.StoreTokens(tokens);
+
+            // 登录
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                currentAuthenticateResult.Principal, currentAuthenticateResult.Properties);
+
+            return tokenResponse.AccessToken;
+        }
+    }
+}
---
+++ 07-08. Hybrid Flow - MVC Client/HybridMvcClient/HybridClient/Startup.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using HybridClient.Auths;
+using HybridClient.Requirements;
+using IdentityModel;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace HybridClient
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                })
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+                    {
+                        options.AccessDeniedPath = "/Authorization/AccessDenied";
+                    })
+                .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+                {
+                    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.Authority = "http://localhost:5000";
+                    options.RequireHttpsMetadata = false;
+
+                    options.ClientId = "hybrid client";
+                    options.ClientSecret = "hybrid secret";
+                    options.SaveTokens = true;
+                    options.ResponseType = "code id_token";
+
+                    options.Scope.Clear();
+
+                    options.Scope.Add("api1");
+                    options.Scope.Add(OidcConstants.StandardScopes.OpenId);
+                    options.Scope.Add(OidcConstants.StandardScopes.Profile);
+                    options.Scope.Add(OidcConstants.StandardScopes.Email);
+                    options.Scope.Add(OidcConstants.StandardScopes.Phone);
+                    options.Scope.Add(OidcConstants.StandardScopes.Address);
+                    options.Scope.Add("roles");
+                    options.Scope.Add("locations");
+
+                    options.Scope.Add(OidcConstants.StandardScopes.OfflineAccess);
+
+                    // 集合里的东西 都是要被过滤掉的属性，nbf amr exp...
+                    options.ClaimActions.Remove("nbf");
+                    options.ClaimActions.Remove("amr");
+                    options.ClaimActions.Remove("exp");
+
+                    // 不映射到User Claims里
+                    options.ClaimActions.DeleteClaim("sid");
+                    options.ClaimActions.DeleteClaim("sub");
+                    options.ClaimActions.DeleteClaim("idp");
+
+                    // 让Claim里面的角色成为mvc系统识别的角色
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        NameClaimType = JwtClaimTypes.Name,
+                        RoleClaimType = JwtClaimTypes.Role
+                    };
+                });
+
+            services.AddAuthorization(options =>
+            {
+                //options.AddPolicy("SmithInSomewhere", builder =>
+                //{
+                //    builder.RequireAuthenticatedUser();
+                //    builder.RequireClaim(JwtClaimTypes.FamilyName, "Smith");
+                //    builder.RequireClaim("location", "somewhere");
+                //});
+                options.AddPolicy("SmithInSomewhere", builder =>
+                    {
+                        builder.AddRequirements(new SmithInSomewareRequirement());
+                    });
+            });
+
+            services.AddSingleton<IAuthorizationHandler, SmithInSomewhereHandler>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
