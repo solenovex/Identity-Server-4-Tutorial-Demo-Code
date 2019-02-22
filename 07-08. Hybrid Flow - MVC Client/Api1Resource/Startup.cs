03b23e665b4960ba26a60cbc5505abf115775f6d
Dave <solenovex@live.com>
2019-02-22 08:33:39 +0800

    update

--- 07-08. Hybrid Flow - MVC Client/Api1Resource/Startup.cs
+++ 07-08. Hybrid Flow - MVC Client/Api1Resource/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer4.AccessTokenValidation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -13,6 +14,8 @@ namespace Api1Resource
 {
     public class Startup
     {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
@@ -21,14 +24,21 @@ namespace Api1Resource
             .AddAuthorization()
             .AddJsonFormatters();
 
-            services.AddAuthentication("Bearer")
-                .AddJwtBearer("Bearer", options =>
+            services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+                .AddIdentityServerAuthentication(options =>
                 {
                     options.Authority = "http://localhost:5000";
+                    options.ApiName = "api1";
                     options.RequireHttpsMetadata = false;
-
-                    options.Audience = "api1";
+                    options.ApiSecret = "api1 secret";
                 });
+                //.AddJwtBearer("Bearer", options =>
+                //{
+                //    options.Authority = "http://localhost:5000";
+                //    options.RequireHttpsMetadata = false;
+
+                //    options.Audience = "api1";
+                //});
 
             services.AddMemoryCache();
 
@@ -47,6 +57,7 @@ namespace Api1Resource
 
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseCors("AngularClientOrigin");
